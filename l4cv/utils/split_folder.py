fa1fa3a8992c5246c7d2b216efe2f59961c6c395
RiveSunder <rivesunder@protonmail.com>
2022-12-15 17:36:53 -0700

    update split_folder helper to check if folders are folders

--- l4cv/utils/split_folder.py
+++ l4cv/utils/split_folder.py
@@ -4,7 +4,7 @@ import argparse
 import numpy as np
 
 
-def split_folder(folder, test_proportion=0.1):
+def split_folder(folder, tag="test_", test_proportion=0.1):
 
     dir_list = os.listdir(folder)
 
@@ -12,7 +12,7 @@ def split_folder(folder, test_proportion=0.1):
     folder_name = folder_split[-1]
     folder_lead = folder_split[0]
 
-    test_folder = os.path.join(folder_lead, f"test_{folder_name}")
+    test_folder = os.path.join(folder_lead, f"{tag}_{folder_name}")
 
     make_test_cmd = f"mkdir {test_folder}"
 
@@ -21,22 +21,23 @@ def split_folder(folder, test_proportion=0.1):
 
     for subfolder in dir_list:
 
-        subfolder_path = os.path.join(f"{test_folder}", subfolder)
-        make_subfolder_cmd = f"mkdir {subfolder_path}"
+        if os.path.isdir(os.path.join(folder,subfolder)):
+            subfolder_path = os.path.join(f"{test_folder}", subfolder)
+            make_subfolder_cmd = f"mkdir {subfolder_path}"
 
-        print(make_subfolder_cmd)
-        os.system(make_subfolder_cmd)
+            print(make_subfolder_cmd)
+            os.system(make_subfolder_cmd)
 
-        file_listdir = os.listdir(os.path.join(folder, subfolder))
-        for filepath in file_listdir:
-            if np.random.rand() < test_proportion:
-                
-                move_from = os.path.join(folder,subfolder,filepath)
-                move_to = os.path.join(f"{test_folder}", subfolder, filepath)
-                mv_cmd = f"mv {move_from} {move_to}"
+            file_listdir = os.listdir(os.path.join(folder, subfolder))
+            for filepath in file_listdir:
+                if np.random.rand() < test_proportion:
+                    
+                    move_from = os.path.join(folder,subfolder,filepath)
+                    move_to = os.path.join(f"{test_folder}", subfolder, filepath)
+                    mv_cmd = f"mv {move_from} {move_to}"
 
-                print(mv_cmd)
-                os.system(mv_cmd)
+                    print(mv_cmd)
+                    os.system(mv_cmd)
 
     print("done")
 
@@ -44,10 +45,11 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input_folder", default="data/trees")
+    parser.add_argument("-t", "--tag", default="test_")
 
     args = parser.parse_args()
 
     folder = args.input_folder
 
-    split_folder(folder)
+    split_folder(folder, tag=args.tag)
 
